c9dc7193c25d5d8c8e6ef69e58401d27c455cc93
Alex Lehman <alexlehman99@proton.me>
2025-03-12 19:58:55 -0400

    Adding better git command, comment, and indent-blankline

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,23 +3,18 @@ return {
   cmd = { "Git", "G"}, -- Load only when running Git commands
   keys = {
     -- Git Status & Commit
-    { "<leader>gs", "<cmd>Git status<CR>", desc = "Git Status" },
     { "<leader>gc", "<cmd>Git commit -v -a<CR>", desc = "Git Commit All" },
     -- Git Push, Pull & Fetch
     { "<leader>gp", "<cmd>Git pull<CR>", desc = "Git Pull" },
     { "<leader>gP", "<cmd>Git push<CR>", desc = "Git Push" },
     { "<leader>gf", "<cmd>Git fetch<CR>", desc = "Git Fetch All" },
-    -- Git Branching
-    { "<leader>gb", "<cmd>Git branch<CR>", desc = "Git Branch List" },
-    { "<leader>gB", "<cmd>Git switch -c ", desc = "Git Create New Branch" },
-    { "<leader>gm", "<cmd>Git merge ", desc = "Git Merge Branch" },
     -- Git Diff & Logs
     { "<leader>gd", "<cmd>Git diff<CR>", desc = "Git Diff" },
-    { "<leader>gl", "<cmd>Git log --oneline --graph --decorate<CR>", desc = "Git Log" },
-    { "<leader>gL", "<cmd>Git log --stat<CR>", desc = "Git Log (Detailed)" },
+    { "<leader>gl", "<cmd>Git log --graph --oneline --abbrev-commit --date=relative --max-count=10<CR>", desc = "Git Log Graph" },
+    { "<leader>gL", "<cmd>Git log --stat --oneline --abbrev-commit --date=relative --max-count=10<CR>",desc = "Git Log" },
     -- Git Reset & Checkout
-    { "<leader>gr", "<cmd>Git restore --staged .<CR>", desc = "Git Unstage All" },
-    { "<leader>gR", "<cmd>Git restore .<CR>", desc = "Git Restore All" },
+    { "<leader>gr", "<cmd>Git restore .<CR>", desc = "Git Restore All" },
+    { "<leader>gR", "<cmd>Git restore --staged .<CR>", desc = "Git Unstage All" },
   },
 }
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,5 +6,8 @@ return {
         { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
         { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+        { "<leader>gb", "<cmd>Telescope git_branches<CR>", desc = "Git Switch Branch" },
+        { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Git Status" },
+        { "<leader>gt", "<cmd>Telescope git_stash<CR>", desc = "Git Stash" },
     },
 }
