7a48c6a7dc74ea33654012eaf07d6f47ebe948d2
Alex Lehman <alexlehman99@proton.me>
2025-03-12 21:08:34 -0400

    Adding better keymaps for switching panes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close Window" })
 
+vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Move to left pane" })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Move to bottom pane" })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Move to top pane" })
+vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Move to right pane" })
+
