39046cf667a0c69d309ff206bf2f12b39776e2c6
Alex Lehman <alexlehman99@proton.me>
2025-03-14 22:44:47 -0400

    Removing extra stuff from treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  lazy = false, -- Load immediately
-  build = ":TSUpdate", -- Automatically update parsers
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "python", "javascript", "c" }, -- Add languages you need
+      ensure_installed = { "lua", "python", "javascript", "c" },
       sync_install = false, -- Install parsers asynchronously
       highlight = { enable = true }, -- Enable syntax highlighting
-      indent = { enable = true }, -- Enable Treesitter-based indentation
     })
   end,
 }
