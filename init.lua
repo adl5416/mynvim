369ba75606e794ee70dfb9194a09995fdeb0e222
Alex Lehman <alexlehman99@proton.me>
2025-03-15 09:58:06 -0400

    Adding LSP to plugins folder

--- init.lua
+++ init.lua
@@ -4,8 +4,6 @@ require("core.keymaps") -- Load keymaps
 
 -- Load config via Lazy.nvim
 require("config.lazy")
--- Load LSP config 
-require("config.lsp")
 
 -- Set the default theme
 vim.cmd("colorscheme carbonfox")
