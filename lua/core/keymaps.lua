05e66bde0d38925903da4c6edbfe3fcabe1f918b
Alex Lehman <alexlehman99@proton.me>
2025-03-29 23:25:19 -0400

    Adding nvim-tmux plugin

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,9 +7,3 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close Window" })
 
--- Adding better keymaps for switching panes
-vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Move to left pane" })
-vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Move to bottom pane" })
-vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Move to top pane" })
-vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Move to right pane" })
-
